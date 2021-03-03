462d2f0352e92c4be1648720c17b7e659aa0ebd6
TetsuHikari <57446204+TetsuHikari@users.noreply.github.com>
2021-03-03 00:14:10 -0300

    feat: Setup redis Cache

---
+++ src/public/js/nav.js
@@ -0,0 +1,14 @@
+var avatar = document.getElementById('photo-avatar');
+const username = document.getElementById('username');
+async function setNavData(user) {
+  avatar.src = user.photoUrl ? user.photoUrl : "http://www.darylroththeatre.com/wp-content/uploads/2018/10/avatar-placeholder.png";
+  username.innerHTML = user.displayName;
+}
+
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    setNavData(user);
+  } else {
+    // No user is signed in.
+  }
+});
--- src/public/js/plantation.js
+++ src/public/js/plantation.js
@@ -2,9 +2,16 @@ var socket = io.connect("http://localhost:3333");
 socket.emit('jooj', { hello: "world" })
 socket.on('dados', (d) => {
   if (d) {
-    console.log(d);
+    const remainingT = document.getElementById('remaining-time')
+    const remainingH = document.getElementById('remaining-humidity')
+    const elapsedW = document.getElementById('elapsed-water')
     const percent = document.getElementById('percent');
     percent.innerHTML = d + "<span>%</span>";
+
+
+    remainingH.innerHTML = (100 - parseInt(d)) + "% restantes para meta";
+    remainingT.innerHTML = "tempo estimado " + 1 + " minutos";
+    elapsedW.innerHTML = "quantidade de água utilizada na ultima irrigação (" + 10 + "L)";
     document.documentElement.style.setProperty('--percent', parseInt(d));
   }
 })
