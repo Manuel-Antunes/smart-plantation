691cef63ea709be5ed8226ce28490f8b040e872e
TetsuHikari <57446204+TetsuHikari@users.noreply.github.com>
2021-03-05 17:45:51 -0300

    fix: nav design update

--- src/public/js/nav.js
+++ src/public/js/nav.js
@@ -1,8 +1,28 @@
-var avatar = document.getElementById('photo-avatar');
-const username = document.getElementById('username');
+var avatar = document.getElementsByClassName('photo-avatar');
+const username = document.getElementsByClassName('fb-username');
+const email = document.getElementsByClassName('fb-email');
+const button = document.getElementById('clickToOpenMenu');
+button.addEventListener('click', () => {
+  const subMenu = document.querySelector('.sub-menu');
+  if (subMenu.classList.contains('active')) {
+    subMenu.classList.remove('active');
+  } else {
+    subMenu.classList.add('active');
+  }
+})
 async function setNavData(user) {
-  avatar.src = user.photoURL ? user.photoURL : "http://www.darylroththeatre.com/wp-content/uploads/2018/10/avatar-placeholder.png";
-  username.innerHTML = user.displayName;
+  for (let i = 0; i < avatar.length; i++) {
+    const e = avatar[i];
+    e.src = user.photoURL ? user.photoURL : "http://www.darylroththeatre.com/wp-content/uploads/2018/10/avatar-placeholder.png";
+  }
+  for (let i = 0; i < username.length; i++) {
+    const e = username[i];
+    e.innerHTML = user.displayName;
+  }
+  for (let i = 0; i < email.length; i++) {
+    const e = email[i];
+    e.innerHTML = user.email;
+  }
 }
 
 firebase.auth().onAuthStateChanged(function (user) {
