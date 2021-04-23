f883e80004963c21a8d5eccf8ed22395629151b5
TetsuHikari <57446204+TetsuHikari@users.noreply.github.com>
2021-04-23 17:10:51 -0300

    asd

--- src/app.js
+++ src/app.js
@@ -17,55 +17,55 @@ const { routePackets } = require('./routers/packeters');
 require('./database');
 
 class App {
-    constructor() {
-        this.app = express();
-        this.server = http.Server(this.app);
-        this.middlewares();
-        this.routes();
-        this.socket();
-        this.mqtt();
-    }
+  constructor() {
+    this.app = express();
+    this.server = http.Server(this.app);
+    this.middlewares();
+    this.routes();
+    this.socket();
+    this.mqtt();
+  }
 
 <<<<<<< HEAD
-    middlewares() {
-        this.app.use(express.json());
-        this.app.use(cors());
-        this.app.use(cp());
-        this.app.use('/public', express.static(path.resolve(__dirname, 'public')));
-        this.app.use(bodyParser.urlencoded({ extended: false }));
-        this.app.use(
-            '/files',
-            express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
-        );
-        this.app.engine(
-            '.hbs',
-            handlebars({
-                defaultLayout: 'main',
-                extname: '.hbs',
-                layoutsDir: path.resolve(__dirname, 'app', 'views', 'layouts'),
-                partialsDir: path.resolve(__dirname, 'app', 'views', 'partials'),
-            })
-        );
-        this.app.set('views', path.resolve(__dirname, 'app', 'views'));
-        this.app.set('view engine', '.hbs');
-        this.app.use((req, res, next) => {
-            req.io = this.io;
-            next();
-        });
-        this.app.use(
-            session({
-                secret: process.env.APP_KEY,
-                resave: true,
-                saveUninitialized: true,
-            })
-        );
-        this.app.use(flash());
-        this.app.use((req, res, next) => {
-            res.locals.error_message = req.flash('error_message');
-            res.locals.success_message = req.flash('success_message');
-            next();
-        });
-    }
+  middlewares() {
+    this.app.use(express.json());
+    this.app.use(cors());
+    this.app.use(cp());
+    this.app.use('/public', express.static(path.resolve(__dirname, 'public')));
+    this.app.use(bodyParser.urlencoded({ extended: false }));
+    this.app.use(
+      '/files',
+      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+    );
+    this.app.engine(
+      '.hbs',
+      handlebars({
+        defaultLayout: 'main',
+        extname: '.hbs',
+        layoutsDir: path.resolve(__dirname, 'app', 'views', 'layouts'),
+        partialsDir: path.resolve(__dirname, 'app', 'views', 'partials'),
+      })
+    );
+    this.app.set('views', path.resolve(__dirname, 'app', 'views'));
+    this.app.set('view engine', '.hbs');
+    this.app.use((req, res, next) => {
+      req.io = this.io;
+      next();
+    });
+    this.app.use(
+      session({
+        secret: process.env.APP_KEY,
+        resave: true,
+        saveUninitialized: true,
+      })
+    );
+    this.app.use(flash());
+    this.app.use((req, res, next) => {
+      res.locals.error_message = req.flash('error_message');
+      res.locals.success_message = req.flash('success_message');
+      next();
+    });
+  }
 =======
   middlewares() {
     this.app.use(express.json());
@@ -108,47 +108,47 @@ class App {
   }
 >>>>>>> master
 
-    routes() {
-        this.app.use(routes);
-        this.app.use(views);
-    }
+  routes() {
+    this.app.use(routes);
+    this.app.use(views);
+  }
 
-    mqtt() {
-        try {
-            this.mosca = new mosca.Server({
-                http: this.app,
-                // eslint-disable-next-line radix
-                port: parseInt(process.env.MQTT_PORT),
-            });
-        } catch (err) {
-            console.log('hey');
-        }
-        this.mosca.io = this.io;
-        this.mosca.on('clientConnected', (p, c) => {
-            console.log(c);
-        });
-        this.mosca.on('published', (p, c) => {
-            routePackets(p, c, this.mosca);
-        });
+  mqtt() {
+    try {
+      this.mosca = new mosca.Server({
+        http: this.app,
+        // eslint-disable-next-line radix
+        port: parseInt(process.env.MQTT_PORT),
+      });
+    } catch (err) {
+      console.log('hey');
     }
+    this.mosca.io = this.io;
+    this.mosca.on('clientConnected', (p, c) => {
+      console.log(c);
+    });
+    this.mosca.on('published', (p, c) => {
+      routePackets(p, c, this.mosca);
+    });
+  }
 
 <<<<<<< HEAD
-    socket() {
-        /**
-         * @type {import('socket.io').Server}
-         */
-        this.io = io(this.server, {
-            cors: {
-                methods: ['GET', 'POST'],
-                allowedHeaders: ['my-custom-header'],
-                credentials: true,
-            },
-        });
-        this.io.on('connect', socket => {
-            startSockets(socket);
-        });
-        defineNamespace(this.io);
-    }
+  socket() {
+    /**
+     * @type {import('socket.io').Server}
+     */
+    this.io = io(this.server, {
+      cors: {
+        methods: ['GET', 'POST'],
+        allowedHeaders: ['my-custom-header'],
+        credentials: true,
+      },
+    });
+    this.io.on('connect', socket => {
+      startSockets(socket);
+    });
+    defineNamespace(this.io);
+  }
 =======
   socket() {
     /**
